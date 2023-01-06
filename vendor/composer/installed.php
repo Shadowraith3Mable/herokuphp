5319cd67e416312302a226a5837492ce6a0941e6
Shadowraith3Mable <21022328@myrp.edu.sg>
2023-01-06 14:38:37 +0800

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/heroku',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8f338950a3750cae9d84a1ffbd964306f4481115',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/heroku' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8f338950a3750cae9d84a1ffbd964306f4481115',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
